13334baed370b85b1c01b331a67121ef0da6685e
Daniel Yaba <daniely@digital.gov.il>
2024-10-06 21:36:26 +0300

    Added outputs internal_ip and service_account

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,17 @@
 output "instance_name" {
-  value = module.compute-vm.name
+  value = module.gke-mgmt.name
   description = "The name of the instance."
 }
 
 output "instance_id" {
-  value = module.compute-vm.id
+  value = module.gke-mgmt.id
   description = "The ID of the instance."
 }
 
+output "instance_internal_ip" {
+  value = module.gke-mgmt.internal_ip
+}
+
+output "instance_service_account" {
+  value = module.gke-mgmt.service_account
+}
